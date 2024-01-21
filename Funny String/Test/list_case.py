2b2ff0ed50aabed3d8d79988b861a41ecfd83686
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-21 20:59:29 +0700

    make list for q&a

---
+++ Funny String/Test/list_case.py
@@ -0,0 +1,33 @@
+list_1_question = ['acxz','bcxz']
+list_1_ans = ['Funny','Not Funny']
+
+list_2_question = ['ivvkxq','ivvkx']
+list_2_ans = ['Not Funny','Not Funny']
+
+list_3_question = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx']
+list_3_ans = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx',]
+
+list_4_question = ['acxz','bcxz','bcxz']
+list_4_ans = ['Funny','Not Funny','bcxz']
+
+list_5_question = ['ivvkxq','ivvkx','bcxz','bcxz']
+list_5_ans = ['Not Funny','Not Funny','bcxz','bcxz']
+
+list_6_question = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx']
+list_6_ans = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx']
+
+list_7_question = ['acxz','bcxz']
+list_7_ans = ['Funny','Not Funny']
+
+list_8_question = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx']
+list_8_ans = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx']
+
+list_9_question = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx']
+list_9_ans = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx',]
+
+list_10_question = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx']
+list_10_ans = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx',]
+
+list_11_question = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx']
+list_12_ans = ['ivvkxq','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx','ivvkx',]
+
