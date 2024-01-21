2df549629759f442d31748bfdd70840a5a6005fa
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-21 10:56:16 +0700

    gen alpha update input2

--- generate_alpha.py
+++ generate_alpha.py
@@ -1,14 +1,17 @@
 import random
 import string
 
-def generate_random_letters(num_letters):
-    if 2 <= num_letters <= 10000:
+def generate_random_letters(num_letters,min,max):
+    if min <= num_letters <= max:
         letters = ''.join(random.choices(string.ascii_letters, k=num_letters))
         return letters
     else:
         return "Number of letters must be between 2 and 10000."
 
 num_letters_input = int(input("Enter the number of letters (2-10000): "))
+
+min = int(input("min: "))
+max = int(input("max: "))
 
 result = generate_random_letters(num_letters_input)
 print(result)
