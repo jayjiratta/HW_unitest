50633076a95f16afd3b843444e34546d36436d43
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-23 09:23:23 +0700

    rename def test case to len from str in list

--- Funny_String/Test/test.py
+++ Funny_String/Test/test.py
@@ -15,63 +15,69 @@ from Funny_String.Test.list_case import (
 )
 
 class TestDataTestCases(unittest.TestCase):
-    def test_list_1(self):
+
+    def test_list_4_4(self):
         user_lst = list_1_question
         if isinstance(user_lst, list):
             result = all(user_lst)
         self.assertListEqual(result, list_1_ans)
-        
 
-        # list_1, list_2 = funnyString(list_1_question)
-        # result = list_check(list_1, list_2)
-        # self.assertListEqual(result, list_1_ans)
+    def test_list_6_5(self):
+        user_lst = list_2_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_2_ans)
 
-    # def test_list_2(self):
-    #     list_1, list_2 = funnyString(list_2_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_2_ans)
+    def test_list_36_5_18_54_98_27_20_33_77_11(self):
+        user_lst = list_3_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_3_ans)
 
-    # def test_list_3(self):
-    #     list_1, list_2 = funnyString(list_3_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_3_ans)
+    def test_list_99_9_10(self):
+        user_lst = list_4_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_4_ans)
 
-    # def test_list_4(self):
-    #     list_1, list_2 = funnyString(list_4_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_4_ans)
+    def test_list_2359_79_12_777(self):
+        user_lst = list_5_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_5_ans)
 
-    # def test_list_5(self):
-    #     list_1, list_2 = funnyString(list_5_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_5_ans)
-
-    # def test_list_6(self):
-    #     list_1, list_2 = funnyString(list_6_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_6_ans)
+    def test_list_956_70_12_1046_17(self):
+        user_lst = list_6_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_6_ans)
     
-    # def test_list_7(self):
-    #     list_1, list_2 = funnyString(list_7_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_7_ans)
+    def test_list_26(self):
+        user_lst = list_7_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_7_ans)
 
-    # def test_list_8(self):
-    #     list_1, list_2 = funnyString(list_8_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_8_ans)
+    def test_list_91_2_7455_2025_12_6500(self):
+        user_lst = list_8_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_8_ans)
 
-    # def test_list_9(self):
-    #     list_1, list_2 = funnyString(list_9_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_9_ans)
+    def test_list_2_70_206_12_4545_5555_12_936_2525(self):
+        user_lst = list_9_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_9_ans)
 
-    # def test_list_10(self):
-    #     list_1, list_2 = funnyString(list_10_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_10_ans)
+    def test_list_4343_945_7_435_9_9_9(self):
+        user_lst = list_10_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_10_ans)
 
-    # def test_list_11(self):
-    #     list_1, list_2 = funnyString(list_11_question)
-    #     result = list_check(list_1, list_2)
-    #     self.assertListEqual(result, list_11_ans)
+    def test_list_476_9_741_852_5_123_7_789(self):
+        user_lst = list_11_question
+        if isinstance(user_lst, list):
+            result = all(user_lst)
+        self.assertListEqual(result, list_11_ans)
