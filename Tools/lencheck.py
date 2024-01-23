4ea584b1c13834dc7ff8656d60937b1e8b72fa2b
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-23 22:04:06 +0700

    add tool that make list from test plan in excel

--- Tools/lencheck.py
+++ Tools/lencheck.py
@@ -1,7 +1,12 @@
-def lencheck(lst):
-    for_name = '_'.join(str(len(s)) for s in lst)
-    return for_name
+# def lencheck(lst):
+#     for_name = '_'.join(str(len(s)) for s in lst)
+#     return for_name
 
-user = input("list: ")
-print(lencheck(eval(user)))
+# user = input("list: ")
+# print(lencheck(eval(user)))
 
+def l(word) :
+    return len(word)
+
+user = input("word")
+print(l(user))
